e6940b222a3584c18c210c144bb394771df8ebb2
charbel-maroun <charbel-maroun@hotmail.com>
2024-02-12 21:33:32 -0500

    Update Screen Size

--- client/src/components/LiveChessTv.js
+++ client/src/components/LiveChessTv.js
@@ -7,7 +7,7 @@ const LiveChessTv = () => {
             <p>Search for players, check out the top 5 players, watch live chess broadcasts, and MORE!</p>
             <iframe title="LiveChessTv" src="https://lichess.org/tv/frame?theme=brown&bg=dark" width="400px" height="444px" allowtransparency="true"></iframe>
         </div>
-
+    
 
 
 
